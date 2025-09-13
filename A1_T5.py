f79665dd87c5673746afac53a8871c7da7603c74
vainorauhala <vaino.rauhala@gmail.com>
2025-09-13 17:53:28 +0300

    Add files via upload

---
+++ A1_T5.py
@@ -0,0 +1,8 @@
+print("Calculate the are of a wall.")
+Feed = input("enter the widht in meters: ")
+Widht = int (Feed)
+Feed = input("enter the height in meters: ")
+Height = int(Feed)
+print(f"widht is {Widht} m and height is {Height} m")
+Area = Widht * Height
+print(f"the wall will be {Area} square meters.")           
---
+++ A1_T6.py
@@ -0,0 +1,6 @@
+
+Feed = input("insert an integer: ")
+Value = int (Feed)
+Remainder = Value % 2
+print(f"Value is {Value}")
+print(f"the remainder is {Remainder} when {Value} is divided by 2.")
