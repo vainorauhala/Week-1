b7c19d39377d596bf5aa92d9e4cab48c326c8a51
vainorauhala <vaino.rauhala@gmail.com>
2025-09-17 11:38:46 +0300

    Update A1_T7.py

--- A1_T7.py
+++ A1_T7.py
@@ -1,7 +1,8 @@
 print("Calculate fuel consumtion.")
-Feed = input("Enter travel distance(kilometers):")
-Kilometers = int (Feed)
-Feed = input("Enter fuel usage(liters):")
-Liters = int (Feed)
+Feed = input("Enter travel distance(kilometers): ")
+Kilometers = int(Feed)
+Feed = input("Enter fuel usage(liters): ")
+Liters = int(Feed)
 Consumption = Liters / Kilometers * 100
-print(f"Fuel consumption is {Consumption} l per 100 km")
+print("Fuel consumption is",Consumption,"l per 100 km.")
+
